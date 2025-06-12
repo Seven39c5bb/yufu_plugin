21069bdcdb88ee62e1eccfa49b33a9ab14463b21
Seven39c5bb <1169462875@qq.com>
2025-06-12 03:30:19 +0100

    test:测试

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import requests
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+@register("yufu", "Seven39c5bb", "这是支持渔夫乐园开发的插件", "1.0.0")
+class Yufu(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
@@ -11,14 +12,17 @@ class MyPlugin(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
     
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.command("llt")
+    async def llt(self, event: AstrMessageEvent):
+        url="https://pics-bed.vercel.app/yufu/llt/1.jpg"
+        try:
+            chain =[
+                Comp.Image.fromURL(url)
+            ]
+            yield event.chain_result(chain) # 发送一条纯文本消息
+        except:
+            yield event.plain_result(f'请求失败，可能是没找到这个图片哦qaq')
+        
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
